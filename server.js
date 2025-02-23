5864e7680c70a29689a30585edf785dc643b5a66
axelmw <axelmw@uia.no>
2025-02-23 23:25:39 +0100

    Fixed server.js to enable JSON parsing

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json()); // Middleware for JSON-parsing
-
-app.listen(PORT, () => {
-  console.log(`Server kjører på http://localhost:${PORT}`);
-});
+app.use(express.json());
 
 const treeRoutes = require("./routes");
 app.use("/api", treeRoutes);
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server kjører på http://localhost:${PORT}`);
+});
