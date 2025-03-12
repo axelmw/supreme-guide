d89576f4d1ab6351f2e35cf184add11d7e81d752
axelmw <axelmw@uia.no>
2025-03-12 14:42:20 +0100

    Oppdatert server.js og lagt til statiske filer for PWA

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.use(express.json());
-
 
 app.use((req, res, next) => {
     console.log(`➡️ [${req.method}] ${req.url}`);
@@ -12,15 +11,16 @@ app.use((req, res, next) => {
     next();
 });
 
-
 const treeRoutes = require("./routes");
 app.use("/api", treeRoutes);
 
+// Server statiske filer fra "public"-mappen
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-    res.send("API is running...");
+// For alle andre requests, send index.html
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server kjører på http://localhost:${PORT}`);
